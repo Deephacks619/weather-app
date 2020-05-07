215fddeee1141d3c8f96747ef57d61b09cb21f82
deephacks619 <deepakkyatham@gmai.com>
2020-05-07 17:36:07 +0530

    weather-app

---
+++ src/App.js
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+import './App.css';
+//import Weather from './Components/Weather'
+import 'bootstrap/dist/css/bootstrap.min.css';
+const api={
+  key:"f0f01058e2cd0f5fc06d7ada85550a2c",
+  base:"https://api.openweathermap.org/data/2.5/"
+}
+function App() {
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
+  console.log(weather.cod)
+  const search = evt => {
+    if (evt.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then(res => res.json())
+        .then(result => {
+          console.log(result);
+          if(result.message==="city not found"){
+            setWeather(result)
+          }
+          else{
+          setWeather(result);
+          setQuery('');
+          }
+        });
+    }
+  }
+
+  const dateBuilder = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
+  }
+
+  return (
+    <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 16) ? 'app warm' : 'app') : 'app'}>
+      <main>
+        <div className="search-box">
+          <input 
+            type="text"
+            className="search-bar"
+            placeholder="Search..."
+            onChange={e => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
+          />
+        </div>
+        {(typeof weather.main != "undefined" ) ? (
+          <div>
+          {(weather.cod===404) ? (
+            "CITY NOT FOUND"
+          ):(
+            <div>
+          <div className="location-box">
+            <div className="location">{weather.name}, {weather.sys.country}</div>
+            <div className="date">{dateBuilder(new Date())}</div>
+          </div>
+          <div className="weather-box">
+            <div className="temp">
+              {Math.round(weather.main.temp)}°c
+            </div>
+            <div className="weather">{weather.weather[0].main}</div>
+          </div>
+        </div>
+          )}
+         </div>) : ('')}
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Weather.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Weather extends Component {
+    render() {
+        return (
+            <div className="container">
+                <div className="cards">
+                <h1>London</h1>
+                   <h5 className="py-4">
+                       
+                   </h5>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Weather;
